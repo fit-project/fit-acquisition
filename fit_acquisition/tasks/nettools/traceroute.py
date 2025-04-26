230cba0c230043506142a28d225fa62709181cdc
Fabio Zito <zitelog@gmail.com>
2025-04-27 00:13:10 +0200

    WPI

--- fit_acquisition/tasks/nettools/traceroute.py
+++ fit_acquisition/tasks/nettools/traceroute.py
@@ -6,26 +6,79 @@
 # SPDX-License-Identifier: GPL-3.0-only
 # -----
 ######
-import os
-from PyQt6.QtCore import QObject, pyqtSignal, QThread
-from common.constants.view.tasks import labels, state, status
-from view.tasks.task import Task
 
-from common.utility import traceroute
-from common.constants import logger
+import os
+from urllib.parse import urlparse
+from contextlib import redirect_stdout
+import scapy.all as scapy
+
+from shiboken6 import isValid
+
+from PySide6.QtCore import QObject, Signal, QThread, QEventLoop, QTimer
+from PySide6.QtWidgets import QMessageBox
+
+from fit_acquisition.task import Task
+from fit_common.gui.utils import State, Status
+from fit_common.gui.error import Error as ErrorView
+from fit_acquisition.lang import load_translations
 
 
 class TracerouteWorker(QObject):
-    finished = pyqtSignal()
-    started = pyqtSignal()
+    finished = Signal()
+    started = Signal()
+    error = Signal(object)
+
+    def __init__(self, parent=None):
+        QObject.__init__(self, parent=parent)
+        self.translations = load_translations()
 
     def set_options(self, options):
         self.url = options["url"]
         self.folder = options["acquisition_directory"]
 
+    def __traceroute(self, url, filename):
+        try:
+            parsed_url = urlparse(url)
+            netloc = parsed_url.netloc
+
+            if not netloc:
+                self.error.emit(
+                    {
+                        "title": self.translations["TRACEROUTE_ERROR_TITLE"],
+                        "message": self.translations["MALFORMED_URL_ERROR"],
+                        "details": "",
+                    }
+                )
+                return
+
+            netloc = netloc.split(":")[0]
+
+            with open(filename, "w") as f:
+                with redirect_stdout(f):
+                    ans, unans = scapy.sr(
+                        scapy.IP(dst=netloc, ttl=(1, 22), id=scapy.RandShort())
+                        / scapy.TCP(flags=0x2),
+                        timeout=10,
+                        verbose=False,
+                    )
+
+                    for snd, rcv in ans:
+                        print(
+                            f"TTL={snd.ttl} IP={rcv.src} TCP_response={isinstance(rcv.payload, scapy.TCP)}"
+                        )
+
+        except Exception as e:
+            self.error.emit(
+                {
+                    "title": self.translations["TRACEROUTE_ERROR_TITLE"],
+                    "message": self.translations["TRACEROUTE_EXECUTION_ERROR"],
+                    "details": str(e),
+                }
+            )
+
     def start(self):
         self.started.emit()
-        traceroute(self.url, os.path.join(self.folder, "traceroute.txt"))
+        self.__traceroute(self.url, os.path.join(self.folder, "traceroute.txt"))
 
         self.finished.emit()
 
@@ -34,7 +87,9 @@ class TaskTraceroute(Task):
     def __init__(self, logger, progress_bar=None, status_bar=None, parent=None):
         super().__init__(logger, progress_bar, status_bar, parent)
 
-        self.label = labels.TRACEROUTE
+        self.translations = load_translations()
+
+        self.label = self.translations["TRACEROUTE"]
 
         self.worker_thread = QThread()
         self.worker = TracerouteWorker()
@@ -42,32 +97,49 @@ class TaskTraceroute(Task):
         self.worker_thread.started.connect(self.worker.start)
         self.worker.started.connect(self.__started)
         self.worker.finished.connect(self.__finished)
+        self.worker.error.connect(self.__handle_error)
 
         self.destroyed.connect(lambda: self.__destroyed_handler(self.__dict__))
 
+    def __handle_error(self, error):
+        error_dlg = ErrorView(
+            QMessageBox.Icon.Critical,
+            error.get("title"),
+            error.get("message"),
+            error.get("details"),
+        )
+        error_dlg.exec()
+
     def start(self):
         self.worker.set_options(self.options)
-        self.update_task(state.STARTED, status.PENDING)
-        self.set_message_on_the_statusbar(logger.TRACEROUTE_STARTED)
+        self.update_task(State.STARTED, Status.PENDING)
+        self.set_message_on_the_statusbar(self.translations["TRACEROUTE_STARTED"])
         self.worker_thread.start()
 
     def __started(self):
-        self.update_task(state.STARTED, status.SUCCESS)
+        self.update_task(State.STARTED, Status.SUCCESS)
         self.started.emit()
 
     def __finished(self):
-        self.logger.info(logger.TRACEROUTE_GET_INFO_URL.format(self.options["url"]))
-        self.set_message_on_the_statusbar(logger.TRACEROUTE_COMPLETED)
-        self.upadate_progress_bar()
+        self.logger.info(
+            self.translations["TRACEROUTE_GET_INFO_URL"].format(self.options["url"])
+        )
+        self.set_message_on_the_statusbar(self.translations["TRACEROUTE_COMPLETED"])
+        self.update_progress_bar()
 
-        self.update_task(state.COMPLETED, status.SUCCESS)
+        self.update_task(State.COMPLETED, Status.SUCCESS)
 
         self.finished.emit()
+
+        loop = QEventLoop()
+        QTimer.singleShot(1000, loop.quit)
+        loop.exec()
 
         self.worker_thread.quit()
         self.worker_thread.wait()
 
     def __destroyed_handler(self, _dict):
-        if self.worker_thread.isRunning():
-            self.worker_thread.quit()
-            self.worker_thread.wait()
+        if hasattr(self, "worker_thread") and isValid(self.worker_thread):
+            if self.worker_thread.isRunning():
+                self.worker_thread.quit()
+                self.worker_thread.wait()
--- fit_acquisition/tests/tasks/nettools/traceroute_test.py
+++ fit_acquisition/tests/tasks/nettools/traceroute_test.py
@@ -1,36 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+######
+# -----
+# Copyright (c) 2023 FIT-Project
+# SPDX-License-Identifier: GPL-3.0-only
+# -----
+######
+
+
 import os
 import sys
 import unittest
-
 import logging
+import time
 
-from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtTest import QSignalSpy
-from common.constants.view.tasks import labels, state, status
+from PySide6 import QtWidgets, QtCore
+from PySide6.QtWidgets import QApplication
+from PySide6.QtTest import QSignalSpy
+
+from fit_common.gui.utils import State, Status
+from fit_common.core.utils import resolve_path
+from fit_acquisition.lang import load_translations
+
+from fit_acquisition.tasks.nettools.traceroute import TaskTraceroute
 
 
-from view.acquisition.acquisition import Acquisition
-from view.tasks.tasks_info import TasksInfo
-from view.tasks.nettools.traceroute import TaskTraceroute
-
-from common.utility import resolve_path
-from common.constants import logger as Logger
-
-from tests.tasks.tasks_ui import Ui_MainWindow
+from fit_acquisition.tests.tasks.tasks_ui import Ui_MainWindow
 
 app = QApplication(sys.argv)
+
+from fit_configurations.logger import LogConfigTools
+import logging.config
+
+logger = logging.getLogger("view.scrapers.web.web")
 
 
 class TaskTracerouteTest(unittest.TestCase):
     folder = ""
     window = None
-    increment = 100
+    translations = load_translations()
 
     @classmethod
     def setUpClass(cls):
+        log_tools = LogConfigTools()
+        log_tools.change_filehandlers_path(cls.folder)
+        logging.config.dictConfig(log_tools.config)
+
         cls.task = TaskTraceroute(
-            logging.getLogger("view.scrapers.web.web"),
+            logger,
             cls.window.progressBar,
             cls.window.statusbar,
         )
@@ -39,68 +56,68 @@ class TaskTracerouteTest(unittest.TestCase):
             "acquisition_directory": cls.folder,
             "url": "https://google.it",
         }
-        cls.intial_progress_bar_value = cls.window.progressBar.value()
-        cls.task.increment = cls.increment
 
-    def test_00_init_headers_task(self):
-        self.assertEqual(self.task.label, labels.TRACEROUTE)
-        self.assertEqual(self.task.state, state.INITIALIZATED)
-        self.assertEqual(self.task.status, status.SUCCESS)
+    def test_00_init_packet_capture_task(self):
+        self.assertEqual(self.task.label, self.translations["TRACEROUTE"])
+        self.assertEqual(self.task.state, State.INITIALIZATED)
+        self.assertEqual(self.task.status, Status.SUCCESS)
+        self.assertEqual(self.task.progress_bar.value(), 0)
 
     def test_01_headers_task(self):
         spy = QSignalSpy(self.task.started)
 
         self.task.start()
 
-        if len(spy) == 0:
+        if spy.count() == 0:
             received = spy.wait(500)
             while received is False:
                 received = spy.wait(500)
 
-        self.assertEqual(len(spy), 1)
+        self.assertEqual(spy.count(), 1)
 
-        self.assertEqual(self.task.state, state.STARTED)
-        self.assertEqual(self.task.status, status.SUCCESS)
+        self.assertEqual(self.task.state, State.STARTED)
+        self.assertEqual(self.task.status, Status.SUCCESS)
 
         self.assertEqual(
             self.task.status_bar.currentMessage(),
-            Logger.TRACEROUTE_STARTED,
+            self.translations["TRACEROUTE_STARTED"],
         )
         self.assertEqual(self.task.progress_bar.value(), 0)
 
         spy = QSignalSpy(self.task.finished)
+        self.task.increment = 100
 
-        if len(spy) == 0:
+        if spy.count() == 0:
             received = spy.wait(500)
             while received is False:
                 received = spy.wait(500)
 
-        self.assertEqual(len(spy), 1)
-        self.assertEqual(self.task.state, state.COMPLETED)
-        self.assertEqual(self.task.status, status.SUCCESS)
+        self.assertEqual(spy.count(), 1)
+        self.assertEqual(self.task.state, State.COMPLETED)
+        self.assertEqual(self.task.status, Status.SUCCESS)
 
         self.assertEqual(
             self.task.status_bar.currentMessage(),
-            Logger.TRACEROUTE_COMPLETED,
+            self.translations["TRACEROUTE_COMPLETED"],
         )
-        self.assertEqual(
-            self.task.progress_bar.value(),
-            (self.intial_progress_bar_value + self.increment),
-        )
+
+        self.assertEqual(self.task.progress_bar.value(), 100)
 
         self.assertTrue(os.path.exists(os.path.join(self.folder, "traceroute.txt")))
 
 
 if __name__ == "__main__":
-    folder = resolve_path("tests/tasks/traceroute_test_folder")
+
+    folder = resolve_path("acquisition/tasks/traceroute_test_folder")
 
     if not os.path.exists(folder):
         os.makedirs(folder)
 
     MainWindow = QtWidgets.QMainWindow()
-
     TaskTracerouteTest.folder = folder
     TaskTracerouteTest.window = Ui_MainWindow()
     TaskTracerouteTest.window.setupUi(MainWindow)
+    TaskTracerouteTest.window.progressBar.setValue(0)
+    MainWindow.show()
 
     unittest.main()
