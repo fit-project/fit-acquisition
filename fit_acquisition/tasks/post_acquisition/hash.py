8200efb321f7f8f37e0f204affaec0f1b2fb3378
Fabio Zito <zitelog@gmail.com>
2025-07-17 01:39:20 +0200

    refactor(hash): completed refactoring to new Task/TaskWorker pattern

--- fit_acquisition/tasks/post_acquisition/hash.py
+++ fit_acquisition/tasks/post_acquisition/hash.py
@@ -7,40 +7,18 @@
 # -----
 ######
 
+import hashlib
 import logging
 import os
-import hashlib
-from shiboken6 import isValid
+
+from fit_common.gui.utils import Status
+
+from fit_acquisition.tasks.task import Task
+from fit_acquisition.tasks.task_worker import TaskWorker
 
 
-from PySide6.QtCore import QObject, Signal, QThread, QEventLoop, QTimer
-
-
-from fit_acquisition.task import Task
-from fit_common.gui.utils import State, Status
-from fit_acquisition.lang import load_translations
-
-
-class HashWorker(QObject):
+class HashWorker(TaskWorker):
     logger = logging.getLogger("hashreport")
-    finished = Signal()
-    started = Signal()
-
-    @property
-    def folder(self):
-        return self._folder
-
-    @folder.setter
-    def folder(self, folder):
-        self._folder = folder
-
-    @property
-    def exclude_list(self):
-        return self._exclude_list
-
-    @exclude_list.setter
-    def exclude_list(self, exclude_list):
-        self._exclude_list = exclude_list
 
     def __calculate_hash(self, filename, algorithm):
         with open(filename, "rb") as f:
@@ -53,13 +31,12 @@ class HashWorker(QObject):
     def start(self):
         self.started.emit()
 
-        files = [f.name for f in os.scandir(self.folder) if f.is_file()]
-        list().append
-        self.exclude_list.append("acquisition.hash")
-        self.exclude_list.append("acquisition.log")
+        files = [f.name for f in os.scandir(self.options["acquisition_directory"]) if f.is_file()]
+        self.options["exclude_list"].append("acquisition.hash")
+        self.options["exclude_list"].append("acquisition.log")
         for file in files:
-            if file not in self.exclude_list:
-                filename = os.path.join(self.folder, file)
+            if file not in self.options["exclude_list"]:
+                filename = os.path.join(self.options["acquisition_directory"], file)
                 file_stats = os.stat(filename)
                 self.logger.info(
                     "========================================================="
@@ -80,63 +57,32 @@ class HashWorker(QObject):
 
 class TaskHash(Task):
     def __init__(self, logger, progress_bar=None, status_bar=None):
-        super().__init__(logger, progress_bar, status_bar)
+        super().__init__(
+            logger,
+            progress_bar,
+            status_bar,
+            label="HASHFILE",
+            worker_class=HashWorker,
+        )
+    
+    @Task.options.getter
+    def options(self):
+        return self._options
 
-        self.translations = load_translations()
+    @options.setter
+    def options(self, options):
+        options["exclude_list"] = list()
+        if "exclude_from_hash_calculation" in options:
+            options["exclude_list"] = options["exclude_from_hash_calculation"]
 
-        self.label = self.translations["HASHFILE"]
-
-        self.worker_thread = QThread()
-        self.worker = HashWorker()
-        self.worker.moveToThread(self.worker_thread)
-        self.worker_thread.started.connect(self.worker.start)
-        self.worker.started.connect(self.__started)
-        self.worker.finished.connect(self.__finished)
-
-        self.destroyed.connect(lambda: self.__destroyed_handler(self.__dict__))
-
+        self._options = options
+    
     def start(self):
-        self.update_task(State.STARTED, Status.PENDING)
-        self.set_message_on_the_statusbar(
-            self.translations["CALCULATE_HASHFILE_STARTED"]
-        )
-        self.worker.folder = self.options["acquisition_directory"]
-        self.worker.exclude_list = list()
-        if "exclude_from_hash_calculation" in self.options:
-            self.worker.exclude_list = self.options["exclude_from_hash_calculation"]
-
-        self.worker_thread.start()
-
-    def __started(self):
-        self.update_task(State.STARTED, Status.SUCCESS)
-        self.started.emit()
-
-    def __finished(self):
-        self.logger.info(
-            self.translations["CALCULATE_HASHFILE_COMPLETED"].format(
+        super().start_task(self.translations["CALCULATE_HASHFILE_STARTED"])
+    
+    def _finished(self, status=Status.SUCCESS, details=""):
+        message = self.translations["CALCULATE_HASHFILE_COMPLETED"].format(
                 Status.SUCCESS.name
-            )
         )
-        self.set_message_on_the_statusbar(
-            self.translations["CALCULATE_HASHFILE_COMPLETED"].format(
-                Status.SUCCESS.name
-            )
-        )
-        self.update_progress_bar()
 
-        self.update_task(State.COMPLETED, Status.SUCCESS)
-
-        self.finished.emit()
-
-        loop = QEventLoop()
-        QTimer.singleShot(1000, loop.quit)
-        loop.exec()
-
-        self.worker_thread.quit()
-        self.worker_thread.wait()
-
-    def __destroyed_handler(self, _dict):
-        if hasattr(self, "worker_thread") and isValid(self.worker_thread):
-            if self.worker_thread.isRunning():
-                self.worker_thread.quit()
-                self.worker_thread.wait()
+        super()._finished(status, details, message)
---
+++ fit_acquisition/tests/tasks/post_acquisition/test_hash.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+######
+# -----
+# Copyright (c) 2023 FIT-Project
+# SPDX-License-Identifier: GPL-3.0-only
+# -----
+######
+
+import logging
+import logging.config
+import os
+
+import pytest
+from fit_common.core.utils import resolve_path
+from fit_common.gui.utils import State, Status
+from fit_configurations.logger import LogConfigTools
+from PySide6.QtWidgets import QMainWindow
+
+from fit_acquisition.lang import load_translations
+from fit_acquisition.tasks.post_acquisition.hash import TaskHash
+from fit_acquisition.tests.tasks.tasks_ui import Ui_MainWindow
+
+translations = load_translations()
+logger = logging.getLogger("view.scrapers.web.web")
+
+
+@pytest.fixture(scope="module")
+def test_folder():
+    folder = resolve_path("acquisition/tasks/hash_test_folder")
+    os.makedirs(folder, exist_ok=True)
+    return folder
+
+
+@pytest.fixture
+def main_window(qtbot):
+    window = QMainWindow()
+    qtbot.addWidget(window)
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+    ui.progressBar.setValue(0)
+    window.show()
+    return window, ui
+
+
+@pytest.fixture
+def task_instance(main_window, test_folder):
+    window, ui = main_window
+
+    log_tools = LogConfigTools()
+    log_tools.set_dynamic_loggers()
+    log_tools.change_filehandlers_path(test_folder)
+    logging.config.dictConfig(log_tools.config)
+
+    task = TaskHash(
+        logger,
+        ui.progressBar,
+        ui.statusbar,
+    )
+
+    task.options = {"acquisition_directory": test_folder}
+    return task, ui
+
+
+def test_init_hash_task(task_instance):
+    task, _ = task_instance
+
+    assert task.label == translations["HASHFILE"]
+    assert task.state == State.INITIALIZATED
+    assert task.status == Status.SUCCESS
+    assert task.progress_bar.value() == 0
+
+
+def test_hash_task(task_instance, qtbot, test_folder):
+    task, ui = task_instance
+    
+    with qtbot.waitSignal(task.finished, timeout=3000):
+        task.start()
+        task.increment = 100
+
+    assert task.state == State.COMPLETED
+    assert task.status == Status.SUCCESS
+    assert ui.statusbar.currentMessage() == translations["CALCULATE_HASHFILE_COMPLETED"].format(task.status.name)
+    assert task.progress_bar.value() == 100
+
+    assert os.path.exists(os.path.join(test_folder, "acquisition.hash"))
